b93ea967aa29b45d8758816ceada7c76eae594f4
EsquivelSebastian <esquivel.sebastian@me.com>
2022-12-18 23:36:26 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+import MovieJson from './movies.json';
+
+import PageWrpper from './PageWrapping';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	let movies = MovieJson;
+
+	
+
+	return (
+
+		<PageWrpper>
+
+			{movies.map(movie =>
+				<Movie
+
+					title={movie.titulo}
+					rating={movie.calificacion}
+					director={movie.director }
+					runTime={movie.duracion }
+					date={movie.fecha }
+					stars= {movie.actores }
+					descripcion={movie.descripcion}
+					img = {movie.img}
+					 />
+			)}
+
+		</PageWrpper>
+
+
+	)
+
 }
 
 export default App;
